f50df71f5107db72e712111975749001ed74f865
Alex Birkett <alex@birkett.co.uk>
2011-09-27 14:25:15 +0200

    Code formatting cleanup

--- src/com/birkettenterprise/phonelocator/service/WakefulIntentService.java
+++ src/com/birkettenterprise/phonelocator/service/WakefulIntentService.java
@@ -24,20 +24,21 @@ import android.content.Intent;
 import android.os.PowerManager;
 
 abstract public class WakefulIntentService extends IntentService {
+	
 	abstract protected void doWakefulWork(Intent intent);
 	
-	private static final String LOCK_NAME_STATIC="com.birkettenterprise.phonelocator.service.WakefulIntentService";
-	private static volatile PowerManager.WakeLock lockStatic=null;
+	private static final String LOCK_NAME_STATIC = "com.birkettenterprise.phonelocator.service.WakefulIntentService";
+	
+	private static volatile PowerManager.WakeLock lockStatic;
 	
 	synchronized private static PowerManager.WakeLock getLock(Context context) {
-		if (lockStatic==null) {
+		if (lockStatic == null) {
 			PowerManager mgr=(PowerManager)context.getSystemService(Context.POWER_SERVICE);
 			
 			lockStatic=mgr.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK,
 														LOCK_NAME_STATIC);
 			lockStatic.setReferenceCounted(true);
 		}
-		
 		return(lockStatic);
 	}
 	
@@ -46,21 +47,16 @@ abstract public class WakefulIntentService extends IntentService {
 		ctxt.startService(i);
 	}
 	
-	/*public static void sendWakefulWork(Context ctxt, Class<?> clsService) {
-		sendWakefulWork(ctxt, new Intent(ctxt, clsService));
-	}*/
-	
 	public WakefulIntentService(String name) {
 		super(name);
 		setIntentRedelivery(true);
 	}
 	
 	@Override
-  public int onStartCommand(Intent intent, int flags, int startId) {
+	public int onStartCommand(Intent intent, int flags, int startId) {
 		if ((flags & START_FLAG_REDELIVERY)!=0) { // if crash restart...
 			getLock(this.getApplicationContext()).acquire();	// ...then quick grab the lock
 		}
-
 		super.onStartCommand(intent, flags, startId);
 		
 		return(START_REDELIVER_INTENT);
