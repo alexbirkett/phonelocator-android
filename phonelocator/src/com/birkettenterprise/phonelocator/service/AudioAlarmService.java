f890a970f4bffd1ccfdfce05d254f2d594f841f8
Alex Birkett <alex@birkett.co.uk>
2012-06-26 10:13:10 +0200

    AND-42 Create alarm service Use raw resource instead of asset Release soundpool when service is destroyed

--- phonelocator/src/com/birkettenterprise/phonelocator/service/AudioAlarmService.java
+++ phonelocator/src/com/birkettenterprise/phonelocator/service/AudioAlarmService.java
@@ -1,22 +1,17 @@
 package com.birkettenterprise.phonelocator.service;
 
-import java.io.IOException;
+import com.birkettenterprise.phonelocator.R;
 
 import android.app.Service;
 import android.content.Intent;
-import android.content.res.AssetFileDescriptor;
-import android.content.res.AssetManager;
 import android.media.AudioManager;
 import android.media.SoundPool;
 import android.os.IBinder;
 import android.util.Log;
 
 public class AudioAlarmService extends Service {
-
-	private static final String ALARM_ASSET_FILE_NAME = "alarm.mp3";
 	
 	private static final String LOG_TAG = "AudioAlarmService";
-
 	
 	private int mStreamId;
 	private SoundPool mSoundPool;
@@ -27,24 +22,7 @@ public class AudioAlarmService extends Service {
 		super.onCreate();
 		mSoundPool = new SoundPool (1, AudioManager.STREAM_ALARM, 0);
 		
-		AssetManager assetManager = getAssets();
-		AssetFileDescriptor fd = null;
-		try {
-			fd = assetManager.openFd(ALARM_ASSET_FILE_NAME);
-			Log.d(LOG_TAG, "opened asset file descriptor");
-			mStreamId = mSoundPool.load(fd, 1);
-			
-			Log.d(LOG_TAG, "stream id is " + mStreamId);
-			
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		} finally {
-			try {
-				fd.close();
-			} catch (IOException e) {
-				// ignore
-			}
-		}
+		mStreamId = mSoundPool.load(this, R.raw.alarm, 1);
 		
 		Log.d(LOG_TAG, "onCreate()");
 	}
@@ -54,6 +32,7 @@ public class AudioAlarmService extends Service {
 		Log.d(LOG_TAG, "onDestroy()");
 		super.onDestroy();
 		stopAlarm();
+		releaseSoundPool();
 	}
 	
 	@Override
@@ -70,13 +49,19 @@ public class AudioAlarmService extends Service {
 	
 	private void startAlarm() {
 		Log.d(LOG_TAG, "startAlarm()");
-		mSoundPool.play (mStreamId, 1.0F, 1.0F, 0, -1, 1.0F);
+		mSoundPool.play(mStreamId, 1.0F, 1.0F, 0, -1, 1.0F);
 		Log.d(LOG_TAG, "playing stream " + mStreamId);
 	}
 	
 	private void stopAlarm() {
 		Log.d(LOG_TAG, "stopping stream " + mStreamId);
 		mSoundPool.stop(mStreamId);
+	}
+	
+	private void releaseSoundPool() {
+		mSoundPool.release();
+		mSoundPool = null;
+		mStreamId = 0;
 	}
 
 }
