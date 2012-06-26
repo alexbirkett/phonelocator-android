f05264384ebac3836a467b6fc4031b5240f134e3
Alex Birkett <alex@birkett.co.uk>
2012-06-26 10:51:14 +0200

    AND-42 Create alarm service I infer from the addition of the setOnLoadCompleteListener in API Level 8 that the SoundPool.load methods don't complete loading before they return. This would explain why the alarm does not sound when the service is first started. Switched to using the MediaPlayer API instead.

--- phonelocator/src/com/birkettenterprise/phonelocator/service/AudioAlarmService.java
+++ phonelocator/src/com/birkettenterprise/phonelocator/service/AudioAlarmService.java
@@ -1,11 +1,15 @@
 package com.birkettenterprise.phonelocator.service;
 
+import java.io.IOException;
+
 import com.birkettenterprise.phonelocator.R;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.AssetFileDescriptor;
 import android.media.AudioManager;
-import android.media.SoundPool;
+import android.media.MediaPlayer;
 import android.os.IBinder;
 import android.util.Log;
 
@@ -14,16 +18,13 @@ public class AudioAlarmService extends Service {
 	private static final String LOG_TAG = "AudioAlarmService";
 	
 	private int mStreamId;
-	private SoundPool mSoundPool;
-	
+	private MediaPlayer mMediaPlayer;
 	
 	@Override
 	public void onCreate() {
 		super.onCreate();
-		mSoundPool = new SoundPool (1, AudioManager.STREAM_ALARM, 0);
-		
-		mStreamId = mSoundPool.load(this, R.raw.alarm, 1);
-		
+		mMediaPlayer = createAndConfigureMediaPlayer(this, R.raw.alarm);
+
 		Log.d(LOG_TAG, "onCreate()");
 	}
 	
@@ -49,19 +50,48 @@ public class AudioAlarmService extends Service {
 	
 	private void startAlarm() {
 		Log.d(LOG_TAG, "startAlarm()");
-		mSoundPool.play(mStreamId, 1.0F, 1.0F, 0, -1, 1.0F);
+		mMediaPlayer.start();
 		Log.d(LOG_TAG, "playing stream " + mStreamId);
 	}
 	
 	private void stopAlarm() {
 		Log.d(LOG_TAG, "stopping stream " + mStreamId);
-		mSoundPool.stop(mStreamId);
+		mMediaPlayer.stop();
 	}
 	
 	private void releaseSoundPool() {
-		mSoundPool.release();
-		mSoundPool = null;
-		mStreamId = 0;
+		mMediaPlayer.release();
 	}
 
+	/*
+	 * Create and configure a media player. Based on public static
+	 * MediaPlayer create(Context context, int resid) in the Android framework.
+	 * MediaPlayer.create can not be used because it calls prepare() before we
+	 * get opportunity to call setAudioStreamType()
+	 */
+	private static MediaPlayer createAndConfigureMediaPlayer(Context context, int resid) {
+		try {
+			AssetFileDescriptor assetFileDescriptor = context.getResources().openRawResourceFd(resid);
+			if (assetFileDescriptor == null) {
+				throw new RuntimeException("missing audio file");
+			}
+
+			MediaPlayer mediaPlayer = new MediaPlayer();
+			mediaPlayer.setDataSource(assetFileDescriptor.getFileDescriptor(), assetFileDescriptor.getStartOffset(), assetFileDescriptor.getLength());
+			assetFileDescriptor.close();
+			
+			mediaPlayer.setAudioStreamType(AudioManager.STREAM_ALARM);
+			mediaPlayer.setLooping(true);
+			
+			mediaPlayer.prepare();
+			return mediaPlayer;
+		} catch (IOException ex) {
+			throw new RuntimeException(ex);
+		} catch (IllegalArgumentException ex) {
+			throw new RuntimeException(ex);
+		} catch (SecurityException ex) {
+			throw new RuntimeException(ex);
+		}
+	    
+	}
 }
