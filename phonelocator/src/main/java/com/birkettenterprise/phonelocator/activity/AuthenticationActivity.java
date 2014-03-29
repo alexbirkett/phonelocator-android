f0e149cdb6a80e8f355d723edcab6bdf04880aff
Alex Birkett <alex@birkett.co.uk>
2014-03-29 10:39:18 +0100

    Remove SignupActivity, add AuthenticationActivity

---
+++ phonelocator/src/main/java/com/birkettenterprise/phonelocator/activity/AuthenticationActivity.java
@@ -0,0 +1,69 @@
+/**
+ * 
+ *  Copyright 2011-2014 Birkett Enterprise Ltd
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ * 
+ */
+
+package com.birkettenterprise.phonelocator.activity;
+
+import com.birkettenterprise.phonelocator.settings.SettingsHelper;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.widget.Toast;
+
+public class AuthenticationActivity extends Activity {
+
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+	@Override
+	public void onPause() {
+		super.onPause();
+	}
+	
+	@Override
+	public void onResume() {
+		super.onResume();
+        //if (isRegistered()) {
+            startDashboardActivity();
+        //}
+    }
+	
+	@Override
+	protected void onDestroy() {
+	    super.onDestroy();
+	}
+
+	private void startDashboardActivity() {
+		Intent intent = new Intent(this, DashboardActivity.class);
+        startActivity(intent);
+		finish();
+	}
+		
+	private boolean isRegistered() {
+		SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+		return SettingsHelper.isRegistered(sharedPreferences);
+	}
+
+	private void toast(int resourceId) {
+		Toast.makeText(this, resourceId, Toast.LENGTH_LONG).show();
+	}
+}
