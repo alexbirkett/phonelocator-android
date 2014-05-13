b0bd2fd76148f3df46feaa4779a75efcd696c53f
Alex Birkett <alex@birkett.co.uk>
2014-05-13 21:54:45 +0200

    Set BASE_URL to https://ten20live.com

--- phonelocator/src/main/java/com/birkettenterprise/phonelocator/utility/Constants.java
+++ phonelocator/src/main/java/com/birkettenterprise/phonelocator/utility/Constants.java
@@ -1,9 +1,12 @@
 package com.birkettenterprise.phonelocator.utility;
 
+import android.net.Uri;
+
 /**
  * Created by alex on 29/03/14.
  */
 public class Constants {
-    //public static final String BASE_URL = "https://ten20live.com";
-    public static final String BASE_URL = "http://192.168.1.108:3001";
+    public static final String BASE_URL = "https://ten20live.com";
+    //public static final String BASE_URL = "http://192.168.1.103:3001";
+    public static final Uri BASE_URI = Uri.parse(BASE_URL);
 }
