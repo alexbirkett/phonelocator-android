b7e75a95b672b7f5393de494a994d62fcbca9f0b
Alex Birkett <alex@birkett.co.uk>
2014-05-14 21:12:32 +0200

    Remove onRetainNonConfigurationInstance callback

--- phonelocator/src/main/java/com/birkettenterprise/phonelocator/controller/HockeyAppController.java
+++ phonelocator/src/main/java/com/birkettenterprise/phonelocator/controller/HockeyAppController.java
@@ -7,18 +7,12 @@ import com.birkett.controllers.Controller;
 
 import net.hockeyapp.android.CrashManager;
 import net.hockeyapp.android.UpdateManager;
-import net.hockeyapp.android.tasks.CheckUpdateTask;
-
-import java.lang.ref.WeakReference;
 
 /**
  * Created by alex on 13/05/14.
  */
 public class HockeyAppController extends Controller {
 
-    private CheckUpdateTask checkUpdateTask;
-
-    private static final String DOWNLOAD_URL = "https://rink.hockeyapp.net/";
     private static final String APP_ID = "3f7ef8dc87d197b81fb86ff41dcc1314";
     private Activity activity;
 
@@ -29,12 +23,6 @@ public class HockeyAppController extends Controller {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         checkForUpdates();
-    }
-
-    @Override
-    public Object onRetainNonConfigurationInstance() {
-        checkUpdateTask.detach();
-        return checkUpdateTask;
     }
 
     @Override
