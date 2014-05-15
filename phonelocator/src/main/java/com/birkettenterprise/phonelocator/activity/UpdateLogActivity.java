59c4f36f647b517f6f13940bb6fd13797f94e86b
Alex Birkett <alex@birkett.co.uk>
2014-05-15 21:16:03 +0200

    Fix UpdateLogActivity Extend ListActivity instead of using ListController Remove support library classes

--- phonelocator/src/main/java/com/birkettenterprise/phonelocator/activity/UpdateLogActivity.java
+++ phonelocator/src/main/java/com/birkettenterprise/phonelocator/activity/UpdateLogActivity.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  *  Copyright 2011-2014 Birkett Enterprise Ltd
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,25 +13,24 @@
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
- * 
+ *
  */
 
 package com.birkettenterprise.phonelocator.activity;
 
 import java.util.Date;
 
-import com.birkett.controllers.ActivityThatSupportsControllers;
-import com.birkett.controllers.ListController;
 import com.birkettenterprise.phonelocator.R;
 import com.birkettenterprise.phonelocator.database.UpdateLogDatabaseContentProvider;
 import com.birkettenterprise.phonelocator.utility.StringUtil;
 
+import android.app.ListActivity;
 import android.content.ContentResolver;
 import android.content.Context;
-import android.support.v4.app.LoaderManager.LoaderCallbacks;
-import android.support.v4.content.CursorLoader;
+import android.app.LoaderManager.LoaderCallbacks;
+import android.content.CursorLoader;
 import android.content.Intent;
-import android.support.v4.content.Loader;
+import android.content.Loader;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
@@ -44,172 +43,161 @@ import android.widget.ResourceCursorAdapter;
 import android.widget.TextView;
 import android.view.ViewGroup;
 
-public class UpdateLogActivity extends ActivityThatSupportsControllers implements LoaderCallbacks<Cursor>{
-    
-	private class ErrorNotResolvedException extends Exception {
-		private static final long serialVersionUID = 1L;
-	}
-	
-	//private static int ID_COLUMN_INDEX = 0;
-	private static int UPDATE_TIMESTAMP_COLUMN_INDEX = 1;
-	private static int ERROR_TYPE_COLUMN_INDEX = 2;
-	private static int ERROR_MESSAGE_COLUMN_INDEX = 3;
-	private static int LOCATION_TIMESTAMP_COLUMN_INDEX = 4;
-	private static int PROVIDER_COLUMN_INDEX = 5;
-	
-	private ListController listController;
-	
-	private ResourceCursorAdapter resourceCursorAdapter;
-    
-	@Override
-    public void onCreate(Bundle savedInstanceState) {
-		// add controllers before you call super.onCreate()
-		
-		listController = new ListController(this);
-		listController.setContentView(R.layout.update_log_activity);
-	
-		addController(listController);
-		super.onCreate(savedInstanceState);
-		
-		setContentView(listController.getView());
-		
+public class UpdateLogActivity extends ListActivity implements LoaderCallbacks<Cursor>{
 
-		resourceCursorAdapter = new ResourceCursorAdapter(UpdateLogActivity.this, R.layout.update_log_list_item, null, true) {
-
-		     
-	        @Override
-	        public View newView(Context context, Cursor cur, ViewGroup parent) {
-	            LayoutInflater li = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-	            return li.inflate(R.layout.update_log_list_item, parent, false);
-	        }
-
-	        @Override
-	        public void bindView(View view, Context context, Cursor cursor) {
-	          String errorType = cursor.getString(ERROR_TYPE_COLUMN_INDEX);
-	 
-	          TextView updateTimestampView = (TextView) view.findViewById(R.id.update_log_update_timestamp);
-	          setTimeStamp(updateTimestampView,cursor,UPDATE_TIMESTAMP_COLUMN_INDEX);
-	          
-        	  TextView errorView = (TextView) view.findViewById(R.id.update_log_error);
-        		  
-	          if (errorType == null) {     	  
-	         	  TextView locationProviderTextView = (TextView) view.findViewById(R.id.update_log_location_provider);
-	        	  locationProviderTextView.setText(cursor.getString(PROVIDER_COLUMN_INDEX));
-	        	  errorView.setVisibility(View.GONE);
-	           	  view.findViewById(R.id.update_log_location_provider_layout).setVisibility(View.VISIBLE);
-	          } else {
-	        	  errorView.setVisibility(View.VISIBLE);   	
-	        	  setError(errorView,cursor, errorType);
-	        	  view.findViewById(R.id.update_log_location_provider_layout).setVisibility(View.GONE);
-	          }
-	        }
-	    };
-   
-        getSupportLoaderManager().initLoader(0, null, this);
-        
-        listController.setListAdapter(resourceCursorAdapter);
+    private class ErrorNotResolvedException extends Exception {
+        private static final long serialVersionUID = 1L;
     }
-	
-	private void setTimeStamp(TextView view, Cursor cursor, int columnIndex) {
-		view.setText(new Date(cursor.getLong(columnIndex)).toLocaleString());
-	}
-	
-	private void setError(TextView view, Cursor cursor, String errorType) {
-		try {
-			String resolvedError = resolveErrorType(errorType);
-			view.setText(resolvedError);
-		} catch (ErrorNotResolvedException e) {
-			String errorMessage = cursor.getString(ERROR_MESSAGE_COLUMN_INDEX);
-			if (StringUtil.isNullOrWhiteSpace(errorMessage)) {
-				errorMessage = errorType;
-			}
-			view.setText(errorMessage);
-		}
-	}
 
-	
-	private String resolveErrorType(String errorType) throws ErrorNotResolvedException {
-		if (errorType.indexOf("IOException") > 0) {
-			return getString(R.string.error_io_exception);
-		} else if (errorType.indexOf("CorruptStreamException") > 0) {
-			return getString(R.string.error_corrupt_stream);
-		} else if (errorType.indexOf("AuthenticationFailedException") > 0) {
-			return getString(R.string.error_authentication_failed);
-		} else if (errorType.indexOf("LocationPollFailedException") > 0) {
-			return getString(R.string.error_location_poll_failed);
-		}  else if (errorType.indexOf("ConnectException") > 0) {
-			return getString(R.string.error_connect_exception);
-		} 
-		
-		throw new ErrorNotResolvedException();
-		
-	}
-    
+    //private static int ID_COLUMN_INDEX = 0;
+    private static int UPDATE_TIMESTAMP_COLUMN_INDEX = 1;
+    private static int ERROR_TYPE_COLUMN_INDEX = 2;
+    private static int ERROR_MESSAGE_COLUMN_INDEX = 3;
+    private static int LOCATION_TIMESTAMP_COLUMN_INDEX = 4;
+    private static int PROVIDER_COLUMN_INDEX = 5;
+
+    private ResourceCursorAdapter resourceCursorAdapter;
+
     @Override
-	protected void onDestroy() {
-	    super.onDestroy();
-	}
-    
+    public void onCreate(Bundle savedInstanceState) {
+        // add controllers before you call super.onCreate()
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.update_log_activity);
+
+        resourceCursorAdapter = new ResourceCursorAdapter(UpdateLogActivity.this, R.layout.update_log_list_item, null, true) {
+            @Override
+            public View newView(Context context, Cursor cur, ViewGroup parent) {
+                LayoutInflater li = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                return li.inflate(R.layout.update_log_list_item, parent, false);
+            }
+
+            @Override
+            public void bindView(View view, Context context, Cursor cursor) {
+                String errorType = cursor.getString(ERROR_TYPE_COLUMN_INDEX);
+
+                TextView updateTimestampView = (TextView) view.findViewById(R.id.update_log_update_timestamp);
+                setTimeStamp(updateTimestampView,cursor,UPDATE_TIMESTAMP_COLUMN_INDEX);
+
+                TextView errorView = (TextView) view.findViewById(R.id.update_log_error);
+
+                if (errorType == null) {
+                    TextView locationProviderTextView = (TextView) view.findViewById(R.id.update_log_location_provider);
+                    locationProviderTextView.setText(cursor.getString(PROVIDER_COLUMN_INDEX));
+                    errorView.setVisibility(View.GONE);
+                    view.findViewById(R.id.update_log_location_provider_layout).setVisibility(View.VISIBLE);
+                } else {
+                    errorView.setVisibility(View.VISIBLE);
+                    setError(errorView,cursor, errorType);
+                    view.findViewById(R.id.update_log_location_provider_layout).setVisibility(View.GONE);
+                }
+            }
+        };
+
+        getLoaderManager().initLoader(0, null, this);
+
+        setListAdapter(resourceCursorAdapter);
+
+    }
+
+    private void setTimeStamp(TextView view, Cursor cursor, int columnIndex) {
+        view.setText(new Date(cursor.getLong(columnIndex)).toLocaleString());
+    }
+
+    private void setError(TextView view, Cursor cursor, String errorType) {
+        try {
+            String resolvedError = resolveErrorType(errorType);
+            view.setText(resolvedError);
+        } catch (ErrorNotResolvedException e) {
+            String errorMessage = cursor.getString(ERROR_MESSAGE_COLUMN_INDEX);
+            if (StringUtil.isNullOrWhiteSpace(errorMessage)) {
+                errorMessage = errorType;
+            }
+            view.setText(errorMessage);
+        }
+    }
+
+
+    private String resolveErrorType(String errorType) throws ErrorNotResolvedException {
+        if (errorType.indexOf("IOException") > 0) {
+            return getString(R.string.error_io_exception);
+        } else if (errorType.indexOf("CorruptStreamException") > 0) {
+            return getString(R.string.error_corrupt_stream);
+        } else if (errorType.indexOf("AuthenticationFailedException") > 0) {
+            return getString(R.string.error_authentication_failed);
+        } else if (errorType.indexOf("LocationPollFailedException") > 0) {
+            return getString(R.string.error_location_poll_failed);
+        }  else if (errorType.indexOf("ConnectException") > 0) {
+            return getString(R.string.error_connect_exception);
+        }
+
+        throw new ErrorNotResolvedException();
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
     @Override
     public void onNewIntent(Intent intent) {
-    	super.onNewIntent(intent);
+        super.onNewIntent(intent);
     }
 
     @Override
     public void onResume() {
-    	super.onResume();
-    	((ResourceCursorAdapter) listController.getListAdapter()).notifyDataSetChanged();
-	}
+        super.onResume();
+        ((ResourceCursorAdapter)getListAdapter()).notifyDataSetChanged();
+    }
 
-	
-	public Loader<Cursor> onCreateLoader(int id, Bundle args) {	
-		return new CursorLoader(this, UpdateLogDatabaseContentProvider.URI, null, null, null, null);
-	}
 
-	public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
-		 resourceCursorAdapter.changeCursor(data);
-		
-	}
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        return new CursorLoader(this, UpdateLogDatabaseContentProvider.URI, null, null, null, null);
+    }
 
-	public void onLoaderReset(Loader<Cursor> loader) {
-		 resourceCursorAdapter.changeCursor(null);
-	}
-	
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        int count = data.getCount();
+        resourceCursorAdapter.changeCursor(data);
 
-		// Handle item selection
-        int i = item.getItemId();
-        if (i == R.id.update_log_menu_item_clear_log) {
-            clearLog();
-            return true;
-        } else if (i == R.id.update_log_menu_item_web_site) {
-            startWebSite();
+    }
 
-            return super.onOptionsItemSelected(item);
-        } else {
-            return super.onOptionsItemSelected(item);
+    public void onLoaderReset(Loader<Cursor> loader) {
+        resourceCursorAdapter.changeCursor(null);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.update_log_menu_item_clear_log:
+                clearLog();
+                return true;
+            case R.id.update_log_menu_item_web_site:
+                startWebSite();
+            default:
+                return super.onOptionsItemSelected(item);
         }
-	}
-	
-	private void clearLog() {
-		 ContentResolver cr = getContentResolver();
-		 cr.delete(UpdateLogDatabaseContentProvider.URI, null, null); 
-	}
-	
-	
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		MenuInflater inflater = getMenuInflater();
-		inflater.inflate(R.menu.update_log_menu, menu);
-		return true;
-	}
+    }
 
-	private void startWebSite() {
-		
-		Intent viewIntent = new Intent("android.intent.action.VIEW",
-				Uri.parse(getString(R.string.website_url)));
-		
-		startActivity(viewIntent);
-	}
+    private void clearLog() {
+        ContentResolver cr = getContentResolver();
+        cr.delete(UpdateLogDatabaseContentProvider.URI, null, null);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.update_log_menu, menu);
+        return true;
+    }
+
+    private void startWebSite() {
+
+        Intent viewIntent = new Intent("android.intent.action.VIEW",
+                Uri.parse(getString(R.string.website_url)));
+
+        startActivity(viewIntent);
+    }
 }
